4261c8f441ef6732558152d2468ae90ffe014240
JiiXu <ji.krochmal@gmail.com>
2022-12-05 15:58:09 +0100

    solve day 3b

--- advent_of_code/aoc2022/day3/day3b.asm
+++ advent_of_code/aoc2022/day3/day3b.asm
@@ -135,9 +135,6 @@ _start:
           mov       rax, SYS_CLOSE
           syscall
 
-; 0 1 2 3 4 5 6 7 8 9 10111213141516171819202122232425262728293031323334353637383940414243444546474849505152535455
-; a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-
 ; solve the problem
 
 ; load three packs
